1259efdc0934ed15dd7ab824ac4df24fb77ddd4f
Bernat Borrás <bernat.borras@adevinta.com>
2021-02-26 12:43:30 +0100

    Fix checkstyle

--- app/src/main/java/com/alorma/puppies/ui/screen/PuppiesListScreen.kt
+++ app/src/main/java/com/alorma/puppies/ui/screen/PuppiesListScreen.kt
@@ -1,14 +1,46 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alorma.puppies.ui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FilterList
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +58,7 @@ import com.alorma.puppies.ui.widget.PuppyItem
 import com.google.android.material.slider.LabelFormatter
 import com.google.android.material.slider.RangeSlider
 import dev.chrisbanes.accompanist.insets.statusBarsPadding
-import java.util.*
+import java.util.Locale
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -133,32 +165,34 @@ private fun Ranger(
     range: Pair<Int, Int>,
     onRangeChanged: (Pair<Int, Int>) -> Unit,
 ) {
-    AndroidView(factory = { context ->
-        val rangeSlider = RangeSlider(context)
+    AndroidView(
+        factory = { context ->
+            val rangeSlider = RangeSlider(context)
 
-        rangeSlider.values = listOf(range.first, range.second).map { it.toFloat() }
-        rangeSlider.stepSize = 1f
-        rangeSlider.valueFrom = range.first.toFloat()
-        rangeSlider.valueTo = range.second.toFloat()
+            rangeSlider.values = listOf(range.first, range.second).map { it.toFloat() }
+            rangeSlider.stepSize = 1f
+            rangeSlider.valueFrom = range.first.toFloat()
+            rangeSlider.valueTo = range.second.toFloat()
 
-        rangeSlider.labelBehavior = LabelFormatter.LABEL_WITHIN_BOUNDS
-        rangeSlider.setLabelFormatter { years ->
-            "${years.toInt()} years"
-        }
-
-        rangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
-            override fun onStartTrackingTouch(slider: RangeSlider) {}
-
-            override fun onStopTrackingTouch(slider: RangeSlider) {}
-        })
-
-        rangeSlider.addOnChangeListener { slider, _, fromUser ->
-            if (fromUser) {
-                onRangeChanged(slider.values[0].toInt() to slider.values[1].toInt())
+            rangeSlider.labelBehavior = LabelFormatter.LABEL_WITHIN_BOUNDS
+            rangeSlider.setLabelFormatter { years ->
+                "${years.toInt()} years"
             }
+
+            rangeSlider.addOnSliderTouchListener(object : RangeSlider.OnSliderTouchListener {
+                override fun onStartTrackingTouch(slider: RangeSlider) {}
+
+                override fun onStopTrackingTouch(slider: RangeSlider) {}
+            })
+
+            rangeSlider.addOnChangeListener { slider, _, fromUser ->
+                if (fromUser) {
+                    onRangeChanged(slider.values[0].toInt() to slider.values[1].toInt())
+                }
+            }
+            rangeSlider
         }
-        rangeSlider
-    })
+    )
 }
 
 @Composable
--- app/src/main/java/com/alorma/puppies/ui/widget/PuppyItem.kt
+++ app/src/main/java/com/alorma/puppies/ui/widget/PuppyItem.kt
@@ -1,6 +1,26 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alorma.puppies.ui.widget
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,7 +31,6 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.alorma.puppies.data.PuppyProvider
 import com.alorma.puppies.ui.base.modifier.primaryClick
 import com.alorma.puppies.ui.model.PuppyItemModel
@@ -22,7 +41,6 @@ fun PuppyItem(
     puppy: PuppyItemModel,
     onClick: () -> Unit,
 ) {
-    val avatarSize = 64.dp
     Box(
         modifier = Modifier.padding(
             start = 8.dp,
@@ -30,7 +48,7 @@ fun PuppyItem(
             end = 8.dp
         )
     ) {
-        //PuppyItemCard(avatarSize, puppy, onClick = onClick)
+        // PuppyItemCard(avatarSize, puppy, onClick = onClick)
         PuppyAvatar(
             modifier = Modifier
                 .height(180.dp)
